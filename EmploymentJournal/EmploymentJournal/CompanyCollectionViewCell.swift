4e3370e2f14631fbafef107045dd4b7781ce9129
Andrew Porter <haporter@me.com>
2017-03-04 11:20:08 -0700

    wireframes (add company form)

---
+++ EmploymentJournal/EmploymentJournal/CompanyCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CompanyCollectionViewCell.swift
+//  EmploymentJournal
+//
+//  Created by Andrew Porter on 3/3/17.
+//  Copyright © 2017 Andrew Porter. All rights reserved.
+//
+
+import UIKit
+
+class CompanyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
