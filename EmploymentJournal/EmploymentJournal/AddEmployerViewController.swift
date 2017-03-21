77cd74c7eb7002c94301ffd89a7e913bd24c0854
Andrew Porter <haporter@me.com>
2017-03-20 22:40:07 -0600

    minimum design of AddEmployerVC

---
+++ EmploymentJournal/EmploymentJournal/AddEmployerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddEmployerViewController.swift
+//  EmploymentJournal
+//
+//  Created by Andrew Porter on 3/20/17.
+//  Copyright © 2017 Andrew Porter. All rights reserved.
+//
+
+import UIKit
+
+class AddEmployerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func cancelButtonTapped(sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
